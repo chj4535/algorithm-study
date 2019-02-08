684c11eeb010215aea5b70f92a180cdd5f65b356
chj4535 <chj4535@ajou.ac.kr>
2019-02-08 17:50:44 +0900

    391 방법 생각중 각 점마다 그 점이 꽉 차 있는지 보는 방법은 어떨까?

--- 391_Perfect_Rectangle.js
+++ 391_Perfect_Rectangle.js
@@ -33,42 +33,46 @@ var isRectangleCover = function(rectangles) {
   rectangles.sort(sortfun);
   console.log(rectangles);
 
-  min=[rectangles[0][0],rectangles[0][1]];
-  max=[rectangles[0][2],rectangles[0][3]];
+  min=rectangles[0];
+  max=rectangles[0];
 
   for(let i=0;i<rec_num;i++){
 
     if(rectangles[i][0]<min[0] || rectangles[i][1]<min[1]){
-      min[0]=rectangles[i][0];
-      min[1]=rectangles[i][1];
+      min=rectangles[i];
     }
 
-    if(rectangles[i][2]>max[0] || rectangles[i][3]>max[1]){
-      max[0]=rectangles[i][2];
-      max[1]=rectangles[i][3];
+    if(rectangles[i][2]>max[2] || rectangles[i][3]>max[3]){
+      max=rectangles[i];
     }
 
     sum+=(rectangles[i][2]-rectangles[i][0])*(rectangles[i][3]-rectangles[i][1]);
-
+    //console.log(sum);
   }
 
   console.log(min);
   console.log(max);
   console.log(sum);
 
-  if(((max[0]-min[0])*(max[1]-min[1]))!=sum){
+  if(((max[2]-min[0])*(max[3]-min[1]))!=sum){
     console.log("false");
     return false;
   }
 
+  rectangles.some(function(element){
+    
+  });
+
+  return true;
 
 };
 
 isRectangleCover(
   rectangles = [
-  [1,1,2,3],
-  [1,3,2,4],
-  [3,1,4,2],
-  [3,2,4,4]
+    [1,1,3,3],
+    [3,1,4,2],
+    [3,2,4,4],
+    [1,3,2,4],
+    [2,3,3,4]
 ]
 )
